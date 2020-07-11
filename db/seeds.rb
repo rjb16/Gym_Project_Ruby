a518d76519b456394169bdc7438e15270bc26532
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-11 13:52:28 +0100

    adding members to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,24 @@ Member.delete_all()
 member1 = Member.new({
     "first_name" => "Elon",
     "last_name" => "Musk",
-    "age" => 36
+    "age" => 49
 })
 
 member1.save()
+
+member2 = Member.new({
+    "first_name" => "Vladimir",
+    "last_name" => "Putin",
+    "age" => 67
+
+})
+
+member2.save()
+
+member3 = Member.new({
+    "first_name" => "Barack",
+    "last_name" => "Obama",
+    "age" => 58
+})
+
+member3.save()
