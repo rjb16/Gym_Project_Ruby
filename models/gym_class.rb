12e2e103b155f59d908cb43f2016b4425923a370
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-11 14:07:02 +0100

    save function for gym_classes

--- models/gym_class.rb
+++ models/gym_class.rb
@@ -1,1 +1,33 @@
 require_relative("../db/sql_runner")
+
+class Gym_class
+
+    attr_reader :name, :date, :price, :id
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @name = options['name']
+        @date = options['date']
+        @price = options['price']
+    end
+
+    def save()
+        sql = "INSERT INTO gym_classes (name, date, price)
+        VALUES
+        ($1, $2, $3)
+        RETURNING id"
+        values = [@name, @date, @price]
+        gym_class = SqlRunner.run(sql, values).first
+        @id = gym_class['id'].to_i
+    end
+
+
+
+
+
+
+
+
+
+
+end
