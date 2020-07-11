8aca99e45f1f660ce78913331d55706f75082049
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-11 13:27:34 +0100

    initialized member class

--- models/member.rb
+++ models/member.rb
@@ -3,5 +3,13 @@ require_relative('../db/sql_runner')
 class Member
 
     attr_reader :first_name, :last_name, :age, :id
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @first_name = options['first_name']
+        @last_name = options['last_name']
+        @age = options['age']
+    end
     
+
      
