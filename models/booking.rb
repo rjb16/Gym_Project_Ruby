e49e0dcb4d3ba3303787c4acb208e2b7dcd96e6a
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-13 10:25:25 +0100

    initialized booking class

--- models/booking.rb
+++ models/booking.rb
@@ -5,4 +5,7 @@ class Booking
     attr_reader :member_id, :gym_class_id, :id
 
     def initialize(options)
-        
+        @id = options['id'].to_i if options['id']
+        @member_id = options['member_id'].to_i
+        @gym_class_id = options['gym_class_id'].to_i
+    end
