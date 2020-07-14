35cd9f6514634fd3d71711f73476a3701bfa50ca
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-14 11:27:25 +0100

    added a create erb to members

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,7 +3,28 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/member.rb' )
 also_reload( '../models/*' )
 
+# Show all members
+
 get '/members' do
     @members = Member.all()
     erb(:"members/index")
+end
+
+# new
+get '/members/new' do
+    erb(:"members/new")
+end
+
+# Show
+get '/members/:id' do
+    @members = Member.find(params['id'].to_i)
+    erb(:"members/show")
+end
+
+
+# create
+post '/members' do
+    @member = Member.new(params)
+    @member.save()
+    erb(:"members/create")
 end
